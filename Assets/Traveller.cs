1e3efaf0b54f8ca291b6c90ad75c19547a01675f
Waynetron <wpetzler@gmail.com>
2021-01-31 10:17:58 +1100

    Added basic strucure for tile scripts

--- Assets/Traveller.cs
+++ Assets/Traveller.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Traveller : MonoBehaviour {
+    public Vector3 direction;
+    public bool canSeeAdjacent = true;
+    List<Vector3> cardinalDirections = new List<Vector3>() {
+        Vector3.forward,
+        Vector3.back,
+        Vector3.left,
+        Vector3.right
+    };
+
+    void Start() {
+        RandomizeDirection();
+    }
+
+    public void RandomizeDirection() {
+        int randomIndex = Random.Range(0, cardinalDirections.Count - 1);
+        direction = cardinalDirections[randomIndex];
+    }
+
     public void Move(Vector3 direction) {
         transform.localPosition = transform.localPosition + direction;
     }
