ce6e79833f0b1420a0625b74d76960291ade3170
Waynetron <wpetzler@gmail.com>
2021-01-30 16:53:25 +1100

    Added Wayne test scene

---
+++ Assets/Traveller.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traveller : MonoBehaviour {
+    public void Move(Vector3 direction) {
+        transform.localPosition = transform.localPosition + direction;
+    }
+}
