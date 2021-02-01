4c51a7da5e3f2399ac4be125bdbe00ca0fa88f90
Waynetron <wpetzler@gmail.com>
2021-02-01 19:30:33 +1100

    Added subtle interactive rotation to map

---
+++ Assets/RotateToFollowMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class RotateToFollowMouse : MonoBehaviour {
+    float strength = 1.35f;
+
+    void Update() {
+        // turns mouse position into a value between 0 and 1
+        Vector2 mouseViewportPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        // adjust to be -0.5 to 0.5 instead of 0 to 1
+        Vector2 normalized = mouseViewportPosition + new Vector2(-0.5f, -0.5f);
+
+        transform.localEulerAngles = new Vector3(
+            normalized.y * -strength,
+            normalized.x * strength,
+            0
+        );
+    }
+}
